8e7134b33e944bcf174793ef227c1af82f94f5ef
Simone Colazzo <colsimo88@gmail.com>
2020-06-25 17:23:34 +0200

    Added actions utils

--- projects/redux-multipurpose/core/src/lib/utils/ws.ts
+++ projects/redux-multipurpose/core/src/lib/utils/ws.ts
@@ -1,13 +1,16 @@
-import { EntityAdapter } from '@reduxjs/toolkit';
+import { EntityAdapter, createAsyncThunk } from '@reduxjs/toolkit';
 
+////////////////// State utils
 type WsSubstate = { data?: any; loading?: boolean; error?: any | null; };
 
-const DEFAULT_INITIAL_STATE: WsSubstate = { data: null, loading: false, error: null };
+const DEFAULT_INITIAL_SUBSTATE: WsSubstate = { data: null, loading: false, error: null };
 
 export class WsState
 {
     [key: string]: WsSubstate;
 }
+
+let initialState: WsState;
 
 export const createWsInitialState = (substates: (WsState | string)[]) => {
     let state: WsState = {};
@@ -15,7 +18,7 @@ export const createWsInitialState = (substates: (WsState | string)[]) => {
     for (let substate of substates)
     {
         if (typeof substate == 'string')
-            state[substate] = DEFAULT_INITIAL_STATE;
+            state[substate] = DEFAULT_INITIAL_SUBSTATE;
         else
         {
             let key = Object.keys(substate)[0];
@@ -28,5 +31,54 @@ export const createWsInitialState = (substates: (WsState | string)[]) => {
         }
     }
 
+    initialState = state;
+
     return state;
+};
+
+////////////////// Actions utils
+
+//Thunk creator helper
+export const prepareThunk = (substate: string, id: string, callback: any) =>
+{
+    return createAsyncThunk(
+        substate + '/' + id,
+        callback
+    );
+};
+
+export const prepareThunkActionReducers = (thunksWithStates: { thunk: any, substate: string, adapter: EntityAdapter<any> }[]) => {
+    let reducers = {};
+
+    for (let storeInfos of thunksWithStates)
+    {
+        let { thunk, substate, adapter } = storeInfos;
+
+        reducers = {
+            ...reducers,
+            [thunk.pending.toString()]: state => {
+                return {
+                    ...state,
+                    [substate]: {
+                        ...initialState[substate],
+                        loading: true
+                    }
+                };
+            },
+            [thunk.fulfilled.toString()]: (state: WsState, action) => {
+                state[substate].loading = false;
+
+                if (adapter != null && adapter != undefined)
+                    adapter.setAll(state[substate].data, action.payload);
+                else
+                    state[substate].data = action.payload;
+            },
+            [thunk.rejected.toString()]: (state, action) => {
+                state[substate].loading = false;
+                state[substate].error = action.error;
+            }
+        };
+    }
+
+    return reducers;
 };
