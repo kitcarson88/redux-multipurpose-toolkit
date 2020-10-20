d12d1c8c9407d83e43253fd5888c4e2fbc8da793
Simone Colazzo <colsimo88@gmail.com>
2020-10-20 11:18:49 +0200

    Updated packages

--- projects/redux-multipurpose/core/src/lib/utils/persist.ts
+++ projects/redux-multipurpose/core/src/lib/utils/persist.ts
@@ -1,13 +1,13 @@
 import { Reducer } from 'redux';
 import { persistReducer } from 'redux-persist';
-import createEncryptor from 'redux-persist-transform-encrypt';
+import { encryptTransform } from 'redux-persist-transform-encrypt';
 
 export const createStoredReducer = (key: string, storage: any, reducer: Reducer) => {
     return persistReducer({ key, storage }, reducer);
 };
 
 export const createSecureStoredReducer = (key: string, encryptKey: string, storage: any, reducer: Reducer) => {
-    const encryptor = createEncryptor({
+    const encryptor = encryptTransform({
         secretKey: encryptKey,
         onError: (error) => {
             console.log("An error occured during secure data save: ", error);
